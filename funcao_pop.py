b0b93ec8e37a91339a6375db1d32c4e5a90e5590
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2024-10-31 16:33:59 -0300

    exemplo de uso da função pop

---
+++ funcao_pop.py
@@ -0,0 +1,12 @@
+# Criando uma lista de números
+numeros = [10, 20, 30, 40, 50]
+
+# Removendo e retornando o último elemento
+ultimo = numeros.pop()
+print(ultimo)
+print(numeros)
+
+# É possível remover um elemento num índice específico, passando o índice como parÂmetro
+segundo = numeros.pop(1)
+print(segundo)
+print(numeros)
